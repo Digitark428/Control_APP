c2d84132937a9bcdd9cc26b2956b55d909a883d3
Digitark428 <lepetitbooth@gmail.com>
2026-05-14 20:25:54 +0400

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,16 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://pnsmmyzghdjtxftflets.supabase.co'
+const supabaseUrl = 'https://pnsmmyzghdjttxftlefs.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBuc21teXpnaGRqdHR4ZnRsZWZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NjU2NDQsImV4cCI6MjA5NDM0MTY0NH0.sjT0cor766tINc2TjDmJfxZ77llYLQbLeqZCiGXFOao'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+    flowType: 'implicit',
+  },
+  global: {
+    fetch: (...args) => fetch(...args),
+  },
+})
